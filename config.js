22a17695b8e5a25fe75cd8735bceba40ca8fdc61
zakaria <zakaria.slimi@etu.usthb.dz>
2025-06-27 00:06:08 +0100

    first

---
+++ config.js
@@ -0,0 +1,28 @@
+var config = {
+
+    texts: [
+        "Leticia",
+        "The most breathtaking, radiant soul I've ever known",
+        "You deserve not just a mountain of gold, but the entire world… still, I’ll keep it safe for you so your beauty doesn’t outshine the sun too soon 😆",
+        "I wish we could have the entire hotel to ourselves — just you and me in our little paradise… but knowing us, we’ll make anywhere feel like home 🥰",
+        "Sending you all the love in my heart on your special day — and every day after",
+        "You deserve love deeper than oceans and happiness brighter than galaxies",
+        "I tried to make it unforgettable 🎂 because your smile is worth every effort",
+        "Happy Birthday to the woman whose beauty, inside and out, makes the world stop for a second (and me every time I look at you 😍)",
+        "♥️ Leticia — my heart, my light, my forever."
+    ],
+
+    imgs: {
+        "心爱的小可爱": "./imgs/xiaokeai.png",
+    },
+    desc: {
+        turn_on: "start",
+        play: "music ?",
+        bannar_coming: "Color ?",
+        balloons_flying: "something missing ?",
+        cake_fadein: "cake?",
+        light_candle: "Candle?",
+        wish_message: "Happy birthday",
+        story: "A MESSAGE FOR YOU",
+    }
+};
